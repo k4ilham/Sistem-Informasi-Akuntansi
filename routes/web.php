8c40f4e3d78f2016353a9d149156b15616a535d2
user72ilham <143789992+user72ilham@users.noreply.github.com>
2024-03-04 17:18:11 +0700

    update middleware

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,35 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\AkunController;
+use Illuminate\Support\Facades\Auth;
 
 
 Auth::routes(); 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-//User
-Route::get('/user/hapus/{id}', [UserController::class, 'destroy']);
-Route::resource('/user', UserController::class);
 
-//Barang
-Route::get('/barang/hapus/{id}', [BarangController::class, 'destroy']);
-Route::resource('/barang', BarangController::class);
+    Route::group(['middleware' => ['role:admin']], function () {
 
-//Supplier
-Route::get('/supplier/hapus/{id}', [SupplierController::class, 'destroy']);
-Route::resource('/supplier', SupplierController::class);
+        //User
+        Route::get('/user/hapus/{id}', [UserController::class, 'destroy']);
+        Route::resource('/user', UserController::class);
 
-//Akun
-Route::get('/akun/hapus/{id}', [AkunController::class, 'destroy']);
-Route::resource('/akun', AkunController::class);
+        //Barang
+        Route::get('/barang/hapus/{id}', [BarangController::class, 'destroy']);
+        Route::resource('/barang', BarangController::class);
+
+        //Supplier
+        Route::get('/supplier/hapus/{id}', [SupplierController::class, 'destroy']);
+        Route::resource('/supplier', SupplierController::class);
+
+        //Akun
+        Route::get('/akun/hapus/{id}', [AkunController::class, 'destroy']);
+        Route::resource('/akun', AkunController::class);
+        
+    });
+
+
 
 
 
